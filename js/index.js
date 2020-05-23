8596dbc4414055ed5661810c565149eefeaf14ca
supreme-chocomint <59235097+supreme-chocomint@users.noreply.github.com>
2020-05-22 18:27:19 -0600

    add song card, shorten graph

--- js/index.js
+++ js/index.js
@@ -238,14 +238,14 @@ function getLineGraphStaticOptions() {
 
 function getBarGraphStaticOptions(labels) {
   return {
+    aspectRatio: 2.5,
     scales: {
       xAxes: [{
         type: 'category',
         stacked: true,
         gridLines: {
           display: false
-        },
-
+        }
       }],
       yAxes: [{
         display: true,
@@ -280,6 +280,7 @@ function addEventOptions(chart) {
       const datasetIndex = element._datasetIndex;
       const dataset = chart.data.datasets[datasetIndex];
       focus(dataset, chart);
+      showInCard(element._index, datasetIndex, chart);
     }
     else undoFocus(chart);
   }
@@ -402,6 +403,30 @@ function setGraphDataUsingIndices(chart, indices, songData, translations) {
 
   chart.data.datasets = datasets;
   chart.update();
+
+}
+
+function showInCard(index, datasetIndex, chart) {
+
+  const name = chart.data.labels[index];
+  const band = chart.data.datasets[datasetIndex].label;
+  const song = window.chartAttributes.songData[band].find(song => song.name == name);
+
+  const nativeName = song.native_name;
+  const danceability = song.features.danceability;
+  const energy = song.features.energy;
+  const valence = song.features.valence;
+  const url = song.external_urls.spotify;
+
+  const card = document.querySelector('#song-card');
+  card.style.display = 'block';
+
+  card.querySelector('.title').innerHTML = name
+  card.querySelector('.subtitle').innerHTML = band + (nativeName == name ? '' : ` / ${nativeName}`);
+  card.querySelector('.valence-value').innerHTML = valence;
+  card.querySelector('.danceability-value').innerHTML = danceability;
+  card.querySelector('.energy-value').innerHTML = energy;
+  card.querySelector('.bottom-right-link').href = url;
 
 }
 
