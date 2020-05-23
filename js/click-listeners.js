158b8c1f7b929e08ec224b2ee5ad961d20995d04
supreme-chocomint <59235097+supreme-chocomint@users.noreply.github.com>
2020-05-22 20:24:11 -0600

    add help modal

--- js/click-listeners.js
+++ js/click-listeners.js
@@ -1,13 +1,29 @@
+// "Sort by" button
 document.querySelector("#sorter").onclick = (event) => {
-    const el = event.srcElement;
-    if (el.innerHTML == "Sort By Valence") {
-        // User asked to sort by valence
-        sortGraphByValence(window.chart);
-        el.innerHTML = "Sort By Release";
-    }
-    else if (el.innerHTML == "Sort By Release") {
-        // User asked to sort by release
-        sortGraphByRelease(window.chart);
-        el.innerHTML = "Sort By Valence";
-    }
+  const el = event.srcElement;
+  if (el.innerHTML == "Sort By Valence") {
+    // User asked to sort by valence
+    sortGraphByValence(window.chart);
+    el.innerHTML = "Sort By Release";
+  }
+  else if (el.innerHTML == "Sort By Release") {
+    // User asked to sort by release
+    sortGraphByRelease(window.chart);
+    el.innerHTML = "Sort By Valence";
+  }
+}
+
+// Modal dismiss
+for (const el of document.querySelectorAll(".modal")) {
+  el.querySelector(".modal-background").onclick = () => {
+    el.classList.remove("is-active");
+  };
+  el.querySelector(".delete").onclick = () => {
+    el.classList.remove("is-active");
+  }
+}
+
+// Help modal activation
+document.querySelector("#launch-help-modal").onclick = () => {
+  document.querySelector("#help-modal").classList.add("is-active");
 }
